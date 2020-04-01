bf0112a8b108ac01c4d5f27349b1f233ec5e4bc4
linzf <mmzp@qq.com>
2020-04-01 10:40:26 +0800

    update: 忽略非 CREATE TABLE 的 SQL 语句块

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ async function generate(sqlFile, templateFile, modelPath) {
     const sqlContent = fs.readFileSync(sqlFile).toString('utf8');
     const sqlContentArr = sqlContent.split('\n\n');
     for (let content of sqlContentArr) {
-        await modelGenerator.generate(content, templateFile, modelPath);
+        if (content.slice(0, 12).toUpperCase() === 'CREATE TABLE') {
+            await modelGenerator.generate(content, templateFile, modelPath);
+        }
     }
 }
 
